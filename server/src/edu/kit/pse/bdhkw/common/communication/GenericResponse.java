59ce9608d15c42778554149a995efc5500fb9ee2
Tarek Wilkening <tarek_wilkening@web.de>
2017-01-21 22:29:13 +0100

    change Object[] to ArrayList<Object>

--- server/src/edu/kit/pse/bdhkw/common/communication/GenericResponse.java
+++ server/src/edu/kit/pse/bdhkw/common/communication/GenericResponse.java
@@ -1,19 +1,22 @@
 package edu.kit.pse.bdhkw.common.communication;
 
+import java.util.ArrayList;
+
 public class GenericResponse extends Response {
-	private Object[] objects;
+	//private Object[] objects;
+	private ArrayList<Object> objects;
 	
 	public GenericResponse(boolean success) {
 		super(success);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Object[] getObjects() {
+	public ArrayList<Object> getObjects() {
 		return objects;
 	}
-
-	public void setObjects(Object[] objects) {
-		this.objects = objects;
+	
+	public void addObject(Object object) {
+		objects.add(object);
 	}
 	
 }
