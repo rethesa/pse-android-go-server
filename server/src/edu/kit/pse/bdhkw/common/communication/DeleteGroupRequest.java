4c7a330a3bd6070d47eaf2a2e2f2281cdbe30a6e
Tarek Wilkening <tarek_wilkening@web.de>
2017-01-22 15:11:59 +0100

    add requests for deleting users/groups

---
+++ server/src/edu/kit/pse/bdhkw/common/communication/DeleteGroupRequest.java
@@ -0,0 +1,43 @@
+package edu.kit.pse.bdhkw.common.communication;
+
+import edu.kit.pse.bdhkw.common.model.SimpleUser;
+import edu.kit.pse.bdhkw.server.model.GroupServer;
+import edu.kit.pse.bdhkw.server.model.ResourceManager;
+
+public class DeleteGroupRequest extends GroupRequest {
+
+	public DeleteGroupRequest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeleteGroupRequest(String senderDeviceId) {
+		super(senderDeviceId);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Response execute() {
+		// Get the user who sent the request
+		SimpleUser user = ResourceManager.getUser(getSenderDeviceId());
+		
+		// Get the target group
+		GroupServer group = ResourceManager.getGroup(getTargetGroupName());
+		
+		// Prepare response
+		Response response;
+		
+		// Check if user is administrator of the group
+		if (group.getMember(user).isAdmin()) {
+			// Delete the group from the database
+			ResourceManager.deleteGroup(group);
+			
+			// Send confirmation
+			response = new Response(true);
+		} else {
+			// User sent illegal request
+			response = new Response(false);
+		}
+		return response;
+	}
+
+}
