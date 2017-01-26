41d500c4041608883032e834e05b9ea36d918999
Theresa Heine <theresaheine@yahoo.de>
2017-01-26 16:24:50 +0100

    implemented registerUser and deleteUserAccount in AccountHandler but first a few changes have to be made in database model and controller

--- client/app/src/main/java/edu/kit/pse/bdhkw/client/controller/objectStructure/AccountHandler.java
+++ client/app/src/main/java/edu/kit/pse/bdhkw/client/controller/objectStructure/AccountHandler.java
@@ -1,7 +1,17 @@
 package edu.kit.pse.bdhkw.client.controller.objectStructure;
 
+import android.database.sqlite.SQLiteDatabase;
+
+import edu.kit.pse.bdhkw.client.communication.UserRequest;
 import edu.kit.pse.bdhkw.client.controller.database.ServiceAllocation;
+import edu.kit.pse.bdhkw.client.controller.database.ServiceGroup;
 import edu.kit.pse.bdhkw.client.controller.database.ServiceUser;
+import edu.kit.pse.bdhkw.client.model.database.DBHelperAllocation;
+import edu.kit.pse.bdhkw.client.model.database.DBHelperAppointment;
+import edu.kit.pse.bdhkw.client.model.database.DBHelperGroup;
+import edu.kit.pse.bdhkw.client.model.database.DBHelperUser;
+import edu.kit.pse.bdhkw.client.model.objectStructure.SimpleUser;
+import edu.kit.pse.bdhkw.client.model.objectStructure.UserComponent;
 import edu.kit.pse.bdhkw.client.model.objectStructure.UserDecoratorClient;
 
 /**
@@ -10,26 +20,36 @@ import edu.kit.pse.bdhkw.client.model.objectStructure.UserDecoratorClient;
 
 public class AccountHandler {
 
+    private SimpleUser simpleUser;
+
+    private ServiceUser sUser;
+    private ServiceAllocation sAlloc;
+    private ServiceGroup sGroup;
+    private ServiceAllocation sApp;
+
     /**
-     * Register a new user. Create a new user object and also save the device id, that not one
-     * user can make infinite accounts. Just one account per device id.
-     * @param userName
-     * @param deviceID
+     * Register a new user. Create a new simple user object and also save the device id, that not one
+     * user can make infinite accounts. Just one account per device id. This will be checked on
+     * the server.
+     * @param userName name of the user for registration
+     * @param deviceId of his mobile phone
      */
-    public void registerUser(String userName, String deviceID) {
-        //TODO
+    public void registerUser(String userName, String deviceId) {
+        simpleUser = new SimpleUser(userName, deviceId);
+        //UserRequest userRequest = new UserRequest(simpleUser);
+        sUser.insertUserData(simpleUser);
     }
 
     /**
-     * Deletes a user in user.db and also deletes all allocations between this user and all his
-     * groups.
+     * User leaves all groups he is member of so all databases of the user will be deleted.
      * @param user to be deleted.
      */
-    public void deleteUserAccount(UserDecoratorClient user) {
-        ServiceUser sUser = null;
-        sUser.deleteUser(user.getUserID());
-        ServiceAllocation sAlloc = null;
-        //public boolean delteUserInAllGrou
+    public void deleteUserAccount(UserComponent user) {
+        //UserRequest userRequest = new UserRequest(user);
+        //sAlloc.deleteAll();
+        //sGroup.deleteAll();
+        //sApp.deleteAll();
+        //sUser.deleteAll();
         //TODO
     }
 
