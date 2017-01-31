ca121d643fc3d0171a97257364fbd3a5cfcb6b40
dennisb <dmb93@hotmail.de>
2017-01-31 15:05:37 +0100

    fixed isregistered to save first start

--- client/app/src/main/java/edu/kit/pse/bdhkw/client/view/MainActivity.java
+++ client/app/src/main/java/edu/kit/pse/bdhkw/client/view/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.kit.pse.bdhkw.client.view;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,25 +10,38 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private final String defaultregistered = "a";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(edu.kit.pse.bdhkw.R.layout.main_activitiy);
 
-        if(isUnregistered()) {
+        //SharedPreferences prefs = this.getSharedPreferences("edu.kit.pse.bdhkq.client", Context.MODE_PRIVATE);
+        //String isregistered = "edu.kit.pse.bdhkq.client.registered_file_key";
+
+        //String defaultregistered = defaultregistered;
+        //if(prefs.edit().putBoolean("registered" ,true).commit()){
+            // write successfull
+        //}
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+        if(isRegistered(prefs)) {
+            startActivity(new Intent(this, GroupActivity.class));
+            prefs.edit().putBoolean("registered", true);
+        } else {
             Intent intent = new Intent(this, UsernameActivity.class);
             intent.putExtra("OpenFirstTime", "true");
             startActivity(new Intent(intent));
-        } else if(!isUnregistered()) {
-            startActivity(new Intent(this, GroupActivity.class));
         }
 
     }
 
-    private boolean isUnregistered() {
+    private boolean isRegistered(SharedPreferences prefs) {
         //TODO: openFirstTime muss irgendwo anders gespeichert sein und geholt werden
-        return true;
+        return prefs.getBoolean("registered", false);
     }
 
     @Override
