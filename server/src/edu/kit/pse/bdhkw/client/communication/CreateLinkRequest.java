0c55618a3e94b75744d5a6d390b723a1e9cda24d
Tarek Wilkening <tarek_wilkening@web.de>
2017-01-31 17:52:04 +0100

    remove unused imports

--- server/src/edu/kit/pse/bdhkw/client/communication/CreateLinkRequest.java
+++ server/src/edu/kit/pse/bdhkw/client/communication/CreateLinkRequest.java
@@ -2,11 +2,6 @@ package edu.kit.pse.bdhkw.client.communication;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
-import edu.kit.pse.bdhkw.common.model.Link;
-import edu.kit.pse.bdhkw.common.model.SimpleUser;
-import edu.kit.pse.bdhkw.server.model.GroupServer;
-import edu.kit.pse.bdhkw.server.model.ResourceManager;
-
 @JsonTypeName("CreateLinkRequest_class")
 public class CreateLinkRequest extends GroupRequest {
 	
