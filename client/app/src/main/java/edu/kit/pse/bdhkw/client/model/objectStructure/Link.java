2e6b25892eb6050c9891a95bde5638d51683dbc5
Matthias Dressel <uiebv@student.kit.edu>
2017-02-07 15:33:07 +0100

    Client: Link: javadoc

--- client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/Link.java
+++ client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/Link.java
@@ -4,19 +4,30 @@ package edu.kit.pse.bdhkw.client.model.objectStructure;
  * Created by Theresa on 13.01.2017.
  */
 
+/**
+ * This class represents a link to invite new users to a group.
+ */
 public class Link {
 
     private String url;
     private String groupName;
     private String secret;
 
+    /**
+     * Instantiates a new Link obejct.
+     *
+     * @param url       the base url
+     * @param groupName the group name
+     * @param secret    the secret; should be generated randomly
+     */
     public Link(String url, String groupName, String secret) {
         this.url = url;
         this.groupName = groupName;
         this.secret = secret;
     }
 
+    @Override
     public String toString() {
-        return this.url + "/"+ this.groupName + "/" + this.secret;
+        return this.url + "/" + this.groupName + "/" + this.secret;
     }
 }
