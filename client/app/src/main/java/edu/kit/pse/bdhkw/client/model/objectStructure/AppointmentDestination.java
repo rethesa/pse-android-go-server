39258e66b94e3074bc509ffa1a1b401c08ecbbb8
Matthias Dressel <uiebv@student.kit.edu>
2017-02-07 15:14:10 +0100

    Client: AppointmentDestination: javadoc

--- client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/AppointmentDestination.java
+++ client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/AppointmentDestination.java
@@ -6,28 +6,54 @@ import org.osmdroid.util.GeoPoint;
  * Created by Theresa on 13.01.2017.
  */
 
+/**
+ * This class represents the location of a group appointment.
+ */
 public class AppointmentDestination {
 
     private String destinationName;
     private GeoPoint destinationPosition;
 
+    /**
+     * Instantiates a new AppointmentDestination object.
+     */
     protected AppointmentDestination() {
         this.destinationName = null;
         this.destinationPosition = null;
     }
 
+    /**
+     * Get the name of the appointments location.
+     *
+     * @return the name of the destination
+     */
     protected String getDestinationName() {
         return destinationName;
     }
 
+    /**
+     * Set the name of the appointments location.
+     *
+     * @param destinationName the name of the destination
+     */
     protected void setDestinationName(String destinationName) {
         this.destinationName = destinationName;
     }
 
+    /**
+     * Get the location/position of the appointment.
+     *
+     * @return the GPS coordinates for the appointment
+     */
     protected GeoPoint getDestinationPosition() {
         return destinationPosition;
     }
 
+    /**
+     * Set the location/position of the appointment.
+     *
+     * @param destinationPosition the GPS coordinates for the appointment
+     */
     protected void setDestinationPosition(GeoPoint destinationPosition) {
         this.destinationPosition = destinationPosition;
     }
