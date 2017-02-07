e10cf6905a1967f0ac45d2e21b10187111103227
Matthias Dressel <uiebv@student.kit.edu>
2017-02-07 15:28:12 +0100

    Client: GpsObject: implement, javadoc

--- client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/GpsObject.java
+++ client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/GpsObject.java
@@ -1,7 +1,5 @@
 package edu.kit.pse.bdhkw.client.model.objectStructure;
 
-import android.graphics.Point;
-
 import org.osmdroid.util.GeoPoint;
 
 import java.util.Date;
@@ -10,29 +8,46 @@ import java.util.Date;
  * Created by Theresa on 20.12.2016.
  */
 
+/**
+ * The class represents the users location at a specific time.
+ */
 public class GpsObject {
 
     private Date timestamp;
     private GeoPoint userPosition;
 
-    public GpsObject() {
+    /**
+     * Instantiates a new GpsObject.
+     *
+     * @param timestamp    time when the location information was taken
+     * @param userPosition position of the user
+     */
+    public GpsObject(Date timestamp, GeoPoint userPosition) {
+        this.timestamp = timestamp;
+        this.userPosition = userPosition;
     }
 
+    /**
+     * Get the position of the user.
+     *
+     * @return the GPS location of the user
+     */
     public GeoPoint getGpsObject() {
-        userPosition = new GeoPoint(50d, 50d);
-        //TODO
         return userPosition;
     }
 
     /**
-     * Time of the GpsObject to see how old it is.
-     * @return
+     * Get the timestamp of the location.
+     *
+     * @return the timestamp of the GPS location
      */
     public Date getTimestamp() {
         return timestamp;
     }
 
-    public void showGpsObjectOnMap(Point position) {
-        //TODO
-    }
+/**    public void showGpsObjectOnMap(GeoPoint position) {
+ // I dont't think this should be here. The GpsObject would have to know the map/activity.
+ // Instead the map should pull the location and display it there.
+ //TODO Abweichung vom Entwurf dokumentieren.
+ }*/
 }
