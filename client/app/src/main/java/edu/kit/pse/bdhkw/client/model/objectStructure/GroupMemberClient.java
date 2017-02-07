becb0562c04ee899b53d362ee8954797b056d5e5
Matthias Dressel <uiebv@student.kit.edu>
2017-02-07 15:38:38 +0100

    Client: GroupMemberClient: javadoc

--- client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/GroupMemberClient.java
+++ client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/GroupMemberClient.java
@@ -4,8 +4,16 @@ package edu.kit.pse.bdhkw.client.model.objectStructure;
  * Created by Theresa on 20.12.2016.
  */
 
+/**
+ * This class represents a user as a normal member of a group.
+ */
 public class GroupMemberClient extends UserDecoratorClient {
 
+    /**
+     * Instantiate a new group member.
+     *
+     * @param name the name of the user
+     */
     public GroupMemberClient(String name) {
         super(name);
     }
