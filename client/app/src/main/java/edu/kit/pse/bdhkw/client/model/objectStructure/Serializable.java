9f5612d18fc738f4e1ee0a36832deeb17204a0ee
Tarek Wilkening <tarek_wilkening@web.de>
2017-02-10 16:42:44 +0100

    add jackson annotations to Serializable

--- client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/Serializable.java
+++ client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/Serializable.java
@@ -3,12 +3,15 @@ package edu.kit.pse.bdhkw.client.model.objectStructure;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+import edu.kit.pse.bdhkw.client.communication.SerializableInteger;
+
 /**
  * Created by Tarek on 10.02.2017.
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "objectType")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value=SimpleUser.class, name="SimpleUser_class")
+        @JsonSubTypes.Type(value=SimpleUser.class, name="SimpleUser_class"),
+        @JsonSubTypes.Type(value=SerializableInteger.class, name="SerializableInteger_class")
 })
 public interface Serializable {
 }
