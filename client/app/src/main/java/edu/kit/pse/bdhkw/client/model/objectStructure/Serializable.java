42e08338716fcf139db7b7aac8d4dd1974e56998
Tarek Wilkening <tarek_wilkening@web.de>
2017-02-18 15:46:43 +0100

    update Serializable interface

--- client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/Serializable.java
+++ client/app/src/main/java/edu/kit/pse/bdhkw/client/model/objectStructure/Serializable.java
@@ -1,18 +1,20 @@
-package edu.kit.pse.bdhkw.client.model.objectStructure;
+package edu.kit.pse.bdhkw.common.model;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-import edu.kit.pse.bdhkw.client.communication.SerializableInteger;
+import edu.kit.pse.bdhkw.server.communication.SerializableInteger;
+import edu.kit.pse.bdhkw.server.communication.SerializableMember;
 
-/**
- * Created by Tarek on 10.02.2017.
- */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "objectType")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value=SimpleUser.class, name="SimpleUser_class"),
-        @JsonSubTypes.Type(value=SerializableInteger.class, name="SerializableInteger_class"),
-        @JsonSubTypes.Type(value=SimpleAppointment.class, name="Appointment_class")
-})
+	@JsonSubTypes.Type(value=SimpleUser.class, name="SimpleUser_class"),
+	@JsonSubTypes.Type(value=SerializableInteger.class, name="SerializableInteger_class"),
+	@JsonSubTypes.Type(value=Appointment.class, name="Appointment_class"),
+	@JsonSubTypes.Type(value=SerializableMember.class, name="SerializableMember_class"),
+	@JsonSubTypes.Type(value=SerializableMember.class, name="SerializableString_class")
+	})
 public interface Serializable {
+	// This is used by Jackson API's objectMapper...
+	// (Intentionally left blank)
 }
