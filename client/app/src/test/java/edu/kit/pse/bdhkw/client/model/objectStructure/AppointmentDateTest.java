33b642ab081bec37963e057cd0a72ef3567730ac
Valansch <mail@maikwild.de>
2017-03-01 13:22:24 +0100

    implemented AppointmentTest but toSimpleAppointment and makeDate are not tested yet. Also fixed as mistake in AppointmentDa teTest

--- client/app/src/test/java/edu/kit/pse/bdhkw/client/model/objectStructure/AppointmentDateTest.java
+++ client/app/src/test/java/edu/kit/pse/bdhkw/client/model/objectStructure/AppointmentDateTest.java
@@ -14,51 +14,51 @@ public class AppointmentDateTest {
 
     @Test
     public void testSetDate() {
-        Appointment appointment = new Appointment();
-        appointment.getAppointmentDate().setDate("24.12.2017");
+        AppointmentDate appointmentDate = new AppointmentDate();
+        appointmentDate.setDate("24.12.2017");
 
-        Assert.assertTrue(appointment.getAppointmentDate().getDate().equals("24.12.2017"));
+        Assert.assertTrue(appointmentDate.getDate().equals("24.12.2017"));
     }
 
     @Test
     public void testSetEmptyDate() {
-        Appointment appointment = new Appointment();
-        appointment.getAppointmentDate().setDate("");
+        AppointmentDate appointmentDate = new AppointmentDate();
+        appointmentDate.setDate("");
 
-        Assert.assertTrue(!appointment.getAppointmentDate().getDate().equals(""));
-        Assert.assertTrue(appointment.getAppointmentDate().getDate().equals("01.01.2000"));
+        Assert.assertTrue(!appointmentDate.getDate().equals(""));
+        Assert.assertTrue(appointmentDate.getDate().equals("01.01.2000"));
     }
 
     @Test
     public void testSetTime() {
-        Appointment appointment = new Appointment();
-        appointment.getAppointmentDate().setTime("14:54");
+        AppointmentDate appointmentDate = new AppointmentDate();
+        appointmentDate.setTime("14:54");
 
-        Assert.assertTrue(appointment.getAppointmentDate().getTime().equals("14:54"));
+        Assert.assertTrue(appointmentDate.getTime().equals("14:54"));
     }
 
     @Test
     public void testSetEmptyDateTime(){
-        Appointment appointment = new Appointment();
-        appointment.getAppointmentDate().setTime("");
+        AppointmentDate appointmentDate = new AppointmentDate();
+        appointmentDate.setTime("");
 
-        Assert.assertTrue(!appointment.getAppointmentDate().getTime().equals(""));
-        Assert.assertTrue(appointment.getAppointmentDate().getTime().equals("00:00"));
+        Assert.assertTrue(!appointmentDate.getTime().equals(""));
+        Assert.assertTrue(appointmentDate.getTime().equals("00:00"));
     }
 
     @Test
     public void testGetDate() {
-        GeoPoint geoPoint = new GeoPoint(1.456, 50.343);
-        Appointment appointment = new Appointment("16.03.2008", "17:53", "Mensa am Adenauerring", geoPoint);
+        AppointmentDate appointmentDate = new AppointmentDate();
+        appointmentDate.setDate("16.03.2008");
 
-        Assert.assertTrue(appointment.getAppointmentDate().getDate().equals("16.03.2008"));
+        Assert.assertTrue(appointmentDate.getDate().equals("16.03.2008"));
     }
 
     @Test
     public void testGetTime() {
-        GeoPoint geoPoint = new GeoPoint(1.456, 50.343);
-        Appointment appointment = new Appointment("16.03.2008", "17:53", "Mensa am Adenauerring", geoPoint);
+        AppointmentDate appointmentDate = new AppointmentDate();
+        appointmentDate.setTime("17:53");
 
-        Assert.assertTrue(appointment.getAppointmentDate().getTime().equals("17:53"));
+        Assert.assertTrue(appointmentDate.getTime().equals("17:53"));
     }
 }
--- client/app/src/test/java/edu/kit/pse/bdhkw/client/model/objectStructure/AppointmentTest.java
+++ client/app/src/test/java/edu/kit/pse/bdhkw/client/model/objectStructure/AppointmentTest.java
@@ -3,11 +3,14 @@ package edu.kit.pse.bdhkw.client.model.objectStructure;
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.osmdroid.util.GeoPoint;
 
 import static org.junit.Assert.*;
 
 /**
  * Test class for appointment.
+ * @author Theresa Heine
+ * @version 1.0
  */
 public class AppointmentTest {
 
@@ -27,23 +30,38 @@ public class AppointmentTest {
         Appointment appointment = new Appointment();
         appointment.setAppointmentDate("24.12.2017", "20:00");
 
-        /*Assert.assertTrue(appointment.getAppointmentDate().getDate().equals("24.12.2017") &&
-        appointment.getAppointmentDate().getTime().equals("20:00"));*/
-        //Assert.assertTrue(appointment.getAppointmentDate().getTime().equals("20:00"));
+        Assert.assertTrue(appointment.getAppointmentDate().getDate().equals("24.12.2017") &&
+        appointment.getAppointmentDate().getTime().equals("20:00"));
     }
 
     @Test
     public void testGetAppointmentDate() {
+        GeoPoint geoPoint = new GeoPoint(8.1234, 50.1234);
+        Appointment appointment = new Appointment("28.02.1997", "16:54", "Karlsruhe", geoPoint);
 
+        Assert.assertEquals("28.02.1997", appointment.getAppointmentDate().getDate());
+        Assert.assertEquals("16:54", appointment.getAppointmentDate().getTime());
     }
 
     @Test
     public void testSetAppointmentDestination() {
+        Appointment appointment = new Appointment();
+        GeoPoint geoPoint = new GeoPoint(1.2222, 40.456);
+        appointment.setAppointmentDestination("Zielort", geoPoint);
+        double delta = 0.0001;
 
+        Assert.assertEquals("Zielort", appointment.getAppointmentDestination().getDestinationName());
+        Assert.assertEquals(1.2222, appointment.getAppointmentDestination().getDestinationPosition().getLatitude(), delta);
+        Assert.assertEquals(40.456,appointment.getAppointmentDestination().getDestinationPosition().getLongitude(), delta);
     }
 
     @Test
     public void testGetAppointmentDestination() {
+        GeoPoint geoPoint = new GeoPoint(8.1234, 50.1234);
+        Appointment appointment = new Appointment("28.02.1997", "16:54", "Karlsruhe", geoPoint);
 
+        Assert.assertEquals("Karlsruhe", appointment.getAppointmentDestination().getDestinationName());
+        Assert.assertEquals(geoPoint.getLatitude(), appointment.getAppointmentDestination().getDestinationPosition().getLatitude());
+        Assert.assertEquals(geoPoint.getLongitude(), appointment.getAppointmentDestination().getDestinationPosition().getLongitude());
     }
 }
