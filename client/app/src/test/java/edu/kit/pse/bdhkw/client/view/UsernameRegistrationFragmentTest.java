df899ea3ee0d90c7cb0b820820bbbb9c0ade5c74
Victoria Karl <victoria.karl@web.de>
2017-03-06 10:52:06 +0100

    try unitTests

---
+++ client/app/src/test/java/edu/kit/pse/bdhkw/client/view/UsernameRegistrationFragmentTest.java
@@ -0,0 +1,22 @@
+package edu.kit.pse.bdhkw.client.view;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Schokomonsterchen on 06.03.2017.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class UsernameRegistrationFragmentTest {
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+
+
+
+}
