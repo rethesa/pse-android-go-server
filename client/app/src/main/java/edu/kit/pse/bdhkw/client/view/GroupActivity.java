5b31756558e86682ec0aecb307ae18834f015623
dennisb <dmb93@hotmail.de>
2017-03-08 18:59:22 +0100

    fixed getting appointment name from server

--- client/app/src/main/java/edu/kit/pse/bdhkw/client/view/GroupActivity.java
+++ client/app/src/main/java/edu/kit/pse/bdhkw/client/view/GroupActivity.java
@@ -113,9 +113,8 @@ public class GroupActivity extends BaseActivity {
                             String stringDate = d.getDay() + "." + d.getMonth() + "." + d.getYear();
                             String stringTime = d.getHours() + ":" + d.getMinutes();
 
-
-                            GeoPoint geoPoint = new GeoPoint(appointment.getDestination().getLongitude(), appointment.getDestination().getLatitude());
-                            GroupClient groupClient = new GroupClient(groupname.getValue(), stringDate, stringTime, "NotOnServer", geoPoint);
+                            GeoPoint geoPoint = new GeoPoint(appointment.getDestination().getLatitude(), appointment.getDestination().getLongitude());
+                            GroupClient groupClient = new GroupClient(groupname.getValue(), stringDate, stringTime, appointment.getName(), geoPoint);
 
                             GroupService groupService = new GroupService(getApplicationContext());
                             groupService.insertNewGroup(groupClient);
